ba0089e62043de467b9731570fab906ab81d05b6
Kevin Ziechmann <kziechmann@gmail.com>
2021-02-08 20:51:16 -0800

    added functionality to upload modal, images submitting to IPFS via infura and displaying on main page!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import ipfsClient from 'ipfs-http-client'
 import { useEffect, useState } from 'react'
-import { Jumbotron, Container } from 'react-bootstrap'
+import { Jumbotron, Container, Card, Badge } from 'react-bootstrap'
 import { UploadModal } from './components/UploadModal'
 import { NavigationBar } from './components/NavigationBar'
 import { EthAccountBar } from './components/EthAccountBar'
@@ -15,6 +15,7 @@ const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' 
 function App() {
   const [ userAccount, setAccount ] = useState('')
   const [ fileTransferContract, setContract ] = useState(null)
+  const [ files, setFiles ] = useState([])
   const [ searchTerm, setSearchTerm ] = useState('')
   const [ inbox, setInbox ] = useState({})
   const [ showModal, setShowModal ] = useState(true)
@@ -38,22 +39,58 @@ function App() {
     setShowModal(!showModal)
   }
 
+  const uploadFileToIPFS = async (fileData) => {
+    const { buffer } = fileData
+    console.log('Uploading file to IPFS....', 'file name:', fileData.fileName, buffer)
+    const result = await ipfs.add(buffer)
+    if(!result){
+      console.error('Error uploading to IPFS')
+    } else {
+      console.log(result)
+      fileData.ipfsHash = result.path
+      setFiles([...files, fileData])
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
           <NavigationBar searchIPFS={searchIPFS} toggleUploadModal={toggleUploadModal}></NavigationBar>
       </header>
-      <UploadModal showModal={showModal} toggleUploadModal={toggleUploadModal}></UploadModal>
+      <UploadModal uploadFileToIPFS={uploadFileToIPFS} showModal={showModal} toggleUploadModal={toggleUploadModal}></UploadModal>
       <main >
         <div className={showModal? 'modal_overlay' : ''}></div>
         <EthAccountBar userAccount={userAccount}></EthAccountBar>
         <Jumbotron fluid>
           <Container>
-            <h1>Videos</h1>
-            <p>
-              This is a modified jumbotron that occupies the entire horizontal space of
-              its parent.
-            </p>
+            {files && files.length? 
+              files.map((file, idx) =>(
+                <Card style={{ width: '18rem' }} key={idx}>
+                  <Card.Img variant="top" src={`https://ipfs.infura.io/ipfs/${file.ipfsHash}`}/>
+                  <div>
+                      <h5>{file.fileName}</h5>
+                      <b>{file.fileType}</b>
+                      <ul className="tag_list">
+                        {file.tags.map((tag, index) => (
+                          <li className="tag">
+                          <Badge key={index} variant="secondary"> 
+                              {tag}
+                          </Badge>
+                          </li>
+                        ))}
+                      </ul>
+                      <span style={{fontSize: 'x-small'}}>{`IPFS CID:${file.ipfsHash}`}</span>
+                  </div>
+                </Card>
+              )) :
+                <div>
+                <h1>Photos</h1>
+                              <p>
+                  This is a modified jumbotron that occupies the entire horizontal space of
+                  its parent.
+                </p>
+                </div>
+            }
           </Container>
         </Jumbotron>
         <Jumbotron fluid>
--- frontend/src/components/UploadModal.js
+++ frontend/src/components/UploadModal.js
@@ -1,9 +1,64 @@
     
-import { Modal, Form } from 'react-bootstrap'
+import { Modal, Form, Alert, Spinner, Badge } from 'react-bootstrap'
+import { useState } from 'react'
 import './UploadModal.css'
 
-export const UploadModal = ({ showModal, toggleUploadModal }) =>{
-    const fileInputName = 'NO FILE SELECTED'
+export const UploadModal = ({ showModal, toggleUploadModal, uploadFileToIPFS }) =>{
+    const [ fileName, setfileName ] = useState('')
+    const [ fileType, setFileType ] = useState('')
+    const [ fileToUpload, setFileToUpload ] = useState(null)
+    const [ buffer, setBufferToUpload ] = useState(null)
+    const [tags, setTags] = useState([]);
+
+    const changeFileType = (type) =>{
+        const newType = fileType === type ? '' : type
+        setFileType(newType)
+    }
+
+    const handleFileSelect = (event)=>{
+        const [ file ] = event.target && event.target.files
+        if(file){
+            setFileToUpload(file)
+            const reader = new window.FileReader()
+            reader.readAsArrayBuffer(file)
+            reader.onloadend = () => {
+                setBufferToUpload(Buffer(reader.result))
+            }
+        } else {
+            setFileToUpload(null)
+            setBufferToUpload(null)
+        }
+    }
+
+    const addTags = event => {
+        if (event.key === "Enter" && event.target.value !== "") {
+            setTags([...tags, event.target.value]);
+            event.target.value = "";
+        }
+    };
+
+    const removeTags = index => {
+        setTags([...tags.filter(tag => tags.indexOf(tag) !== index)]);
+    };
+
+    const handleFileUpload = async (e)=>{
+        e.preventDefault()
+        const fileData = {
+            fileName,
+            fileType,
+            fileToUpload,
+            buffer,
+            tags
+        }
+        toggleUploadModal()
+        setfileName('')
+        setFileType('')
+        setFileToUpload(null)
+        setBufferToUpload(null)
+        setTags([])
+        await uploadFileToIPFS(fileData)
+    }
+
 
     return showModal ? 
         <Modal.Dialog className="upload_modal">
@@ -14,25 +69,74 @@ export const UploadModal = ({ showModal, toggleUploadModal }) =>{
             <Modal.Body>
                 <Form>
                     <Form.Group>
+                        {/* File Name Input */}
                         <Form.Label className="input_label">File Name:</Form.Label>
-                        <Form.Control type="text" placeholder="File Name" />
+                        <Form.Control onChange={e =>{ setfileName(e.target.value.trim())}}  type="text" placeholder="File Name" />
+                        {/* Tags Input */}
+                        <Form.Label className="input_label">Add Tags:</Form.Label>
+                        <div className="tags-input">
+                            <ul className="tag_list">
+                                {tags.map((tag, index) => (
+                                    <li key={index} className="tag">
+                                    <Badge  variant="secondary"> 
+                                        {tag}
+                                        <span className="remove_tag" onClick={() => removeTags(index)} >(x)</span>
+                                    </Badge>
+                                    </li>
+                                ))}
+                            </ul>
+                            <input
+                                type="text"
+                                disabled={tags && tags.length > 10}
+                                style={tags && tags.length > 10? {border: '1px solid grey'} : {}}
+                                onKeyUp={event => event.key === "Enter" ? addTags(event) : null}
+                                placeholder="Press enter to add tags"
+                            />
+                        </div>
+                        {
+                            tags && tags.length > 10?
+                            <Alert style={{marginTop: 10}} variant={'warning'}>
+                                only ten tags allowed per file
+                            </Alert>   
+                            : ''
+                        } 
+                        {/* File Type Input */}
                         <Form.Label className="input_label">File Type:</Form.Label>
-                        <Form.Check inline label="Video" type="checkbox" id='check_input_video' />
-                        <Form.Check inline label="Audio" type="checkbox" id='check_input_audio' />
-                        <Form.Check inline label="Image" type="checkbox" id='check_input_image' />
+                        <Form.Check inline label="Video" onChange={e =>{ changeFileType('video')}} disabled={fileType && fileType !=='video'} type="checkbox" id='check_input_video' />
+                        <Form.Check inline label="Audio" onChange={e =>{ changeFileType('audio')}} disabled={fileType && fileType !=='audio'}type="checkbox" id='check_input_audio' />
+                        <Form.Check inline label="Image" onChange={e =>{ changeFileType('image')}} disabled={fileType && fileType !=='image'}type="checkbox" id='check_input_image' />
+                        {/* File Browser Input */}
                         <Form.Label className="input_label">File To Upload:</Form.Label>
-                        <label style={{display: 'flex'}}>
-                            <Form.File style={{display: 'none'}} label="" />
-                            <button onClick={e => e.preventDefault()} role="button" className="select_file"> SELECT FILE </button>
-                            <div className='file_input_name'> {fileInputName} </div>
+                        <label className="file_input">
+                            <Form.File onChange={handleFileSelect} style={{display: 'none'}} label="" />
+                            <div role="button" className="select_file"> SELECT FILE </div>
+                            { fileToUpload && fileToUpload.name && !buffer ?
+                            <Spinner animation="border" role="status">
+                                <span className="sr-only">Loading...</span>
+                            </Spinner> : ''
+                            }
+                            <div className='file_input_name'> {fileToUpload && fileToUpload.name || "NO FILE SELECTED"} </div>
                         </label>
                     </Form.Group>
                 </Form>
+
+                {
+                !fileName ||!fileName.length > 8 || !buffer || !buffer.length?
+                 <Alert variant={'warning'}>
+                    Please add name and select file before uploading!
+                 </Alert>   
+                 : ''
+                } 
             </Modal.Body>
 
             <Modal.Footer>
                 <button onClick={toggleUploadModal} className="close_btn">Close</button>
-                <button className="secondary">Upload</button>
+                <button 
+                    onClick={handleFileUpload}
+                    disabled={!fileName ||!fileName.length > 8 || !buffer || !buffer.length}
+                    style={!fileName ||!fileName.length > 8 || !buffer || !buffer.length? { backgroundColor: 'lightgrey'} : {}}
+                    className="secondary">Upload
+                </button>
             </Modal.Footer>
         </Modal.Dialog> :
         ''
